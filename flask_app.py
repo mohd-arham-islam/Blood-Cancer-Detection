ff3b0302c2d3081cdab2dba841e690a9453f4af6
Arham Islam <arhamislam808@gmail.com>
2023-10-24 21:51:56 +0530

    Docker File

--- flask_app.py
+++ flask_app.py
@@ -28,24 +28,5 @@ def predict():
         'Confidence': confidence
     }
 
-    # pil_image = Image.open(image)
-    # resizedImg = pil_image.resize((224, 224))
-    
-    # arr = np.array(resizedImg)
-    # # arr = arr / 255.0
-    # batchedImg = np.expand_dims(arr, 0)
-    # prediction = model.predict(batchedImg)[0]
-    # classNames = ['Benign', 'Early', 'Pre', 'Pro']
-    # pos = np.argmax(prediction)
-    # # image = image/255.0
-    # # img = Image.open(image)
-    # # img = img.resize((224, 224))
-    # # img = img_to_array(img)/255.0
-    # # img = img.reshape(1, 224, 224, 3)
-    return {
-        'Class': classNames[pos],
-        'Confidence': round(prediction[pos]*100, 1)
-    }
-
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
