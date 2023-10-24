2a0f4fa8b9653ee0a09d5ccc6f2a628237c6ed14
Arham Islam <arhamislam808@gmail.com>
2023-10-24 16:01:01 +0530

    Prediction pipeline

---
+++ src/pipeline/prediction.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from src import logger, customException
+import numpy as np
+from PIL import Image
+from dataclasses import dataclass
+from src.utils import loadObject
+
+classNames = ['Benign', 'Early', 'Pre', 'Pro']
+
+@dataclass()
+class PredictionConfig:
+    modelPath = os.path.join('artifacts/MLFlow_model.pkl')
+
+class imageFile:
+    def __init__(self, file):
+        self.file = file
+    
+    def getArr(self):
+        try:
+            pilImg = Image.open(self.file)
+            pilImg = pilImg.resize((224, 224))
+            arr = np.array(pilImg)
+            arr = arr/255.0
+
+            return arr
+        
+        except Exception as e:
+            raise customException(e, sys)
+        
+class PredictionPipeline:
+    def __init__(self):
+        self.predictionConfig = PredictionConfig()
+
+    def predict(self, arr):
+        modelPath = self.predictionConfig.modelPath
+        model = loadObject(filePath=modelPath)
+        logger.info('Loaded Model')
+
+        batchedImg = np.expand_dims(arr, 0)
+        batchedImg = batchedImg.astype('float32')
+        logger.info('Created batched image')
+
+        prediction = model.predict(batchedImg)[0]
+        logger.info('Prediction Complete')
+
+        classNames = ['Benign', 'Early', 'Pre', 'Pro']
+        pos = np.argmax(prediction)
+
+        className = classNames[pos]
+        confidence = round(prediction[pos]*100, 1)
+
+        return (
+            className,
+            confidence
+        )
